c98e63642930fb7e9035b7436cd884fd24f84bd3
Yaojiek11 <yksubst6274@gmail.com>
2024-06-04 18:18:31 +0900

    add code

---
+++ TestAdapter/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+/* 継承を利用したAdapter */
+namespace ClassAdapterPattern
+{
+    // Mainクラス
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductPrice p1 = new ProductAdapter(100);
+            ProductPrice p2 = new ProductAdapter(200);
+            int price1 = p1.getPrice();
+            int price2 = p2.getPrice();
+            Console.WriteLine("price1 = {0}",price1);
+            Console.WriteLine("price2 = {0}",price2);
+        }
+    }
+
+    // Productクラス(Adaptee)
+    public class Product
+    {
+        private int cost;
+        public Product(int cost){
+            this.cost = cost;
+        }
+        public int getCost(){
+            return cost;
+        }
+    }
+
+    // ProductPriceインターフェース(Target)
+    public interface ProductPrice
+    {
+        public int getPrice();
+    }
+
+    // ProductAdapterクラス(Adapter)
+    public class ProductAdapter : Product, ProductPrice
+    {
+        public ProductAdapter(int num) : base(num) {}
+        public int getPrice(){
+            return this.getCost();
+        }
+    }
+}
---
+++ delegate1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace delegate1
+{
+    delegate DateTime MyDelegate(DateTime dt, int n);
+
+    class MyClass1
+    {
+        public DateTime Calc(DateTime d, int n){
+            return d.AddDays(n);
+        }
+    }
+
+    class MyClass2
+    {
+        public DateTime Calc(DateTime d, int n)
+        {
+            return d.AddHours(n);
+        }
+    }
+
+    class MyClass3
+    {
+        public DateTime Calc(DateTime d, int n)
+        {
+            return d.AddMinutes(n);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyClass1 mc1 = new MyClass1();
+            MyClass2 mc2 = new MyClass2();
+            MyClass3 mc3 = new MyClass3();
+            
+            MyDelegate md = new MyDelegate(mc1.Calc);
+            DateTime dt = DateTime.Now;
+
+            DateTime mydate;
+
+            mydate = md(dt, 100);
+            Console.WriteLine("今日から100日後は、{0}です", mydate.ToShortDateString());
+
+            md = new MyDelegate(mc2.Calc);
+            mydate = md(dt, 24);
+            Console.WriteLine("今から24時間後は、{0}です", mydate);
+
+            md += new MyDelegate(mc3.Calc);
+            Console.WriteLine("今から100時間後は、{0}です", mydate);
+            mydate = md(dt, 100);
+            Console.WriteLine("今から100分後は、{0}です", mydate);
+        }
+    }
+}
